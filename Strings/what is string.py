b49a5734d6213f0bb62b0bec43234149e39f4c30
home <kanth.mallampalli@gmail.com>
2019-03-14 21:54:39 +0530

    string final

--- Strings/what is string.py
+++ Strings/what is string.py
@@ -2,6 +2,21 @@
 a string is list of char,index of string starts from 0
  individuval characters from string can be retrived by using slicing function with the help of index
 strings are immutable
+
+isallnum() finds any special char
+isalpha () finds contains only alpha or not
+is digits() finds string contains oly digits or not
+is lower() finds lowers case
+is upper () finds upper case
+is space() string contains only white space
+loweer() string to lower case
+upper () string to upper case
+lstrip() remover spaces from left side
+r strip() removers spaces from right side
+strip() remover space from all sides
+endswith ()
+starts with()
+
 '''
 
 s='uday'
@@ -26,4 +41,8 @@ print(s.endswith('t'))
 
 
 print(s.startswith('u'))
+
+s="7676IIHg"
+
+print(s.isalnum())
 
