8ad69c2c9a24d30e6bbb61dbe4f81e02da159095
home <kanth.mallampalli@gmail.com>
2019-03-14 21:45:24 +0530

    String questions

---
+++ Strings/what is string.py
@@ -0,0 +1,29 @@
+'''
+a string is list of char,index of string starts from 0
+ individuval characters from string can be retrived by using slicing function with the help of index
+strings are immutable
+'''
+
+s='uday'
+
+print(s[1:3])
+
+
+t ="%%*T$"
+y= t.isalpha()
+print(y)
+
+
+u="589"
+x=u.isdigit()
+print(x)
+z="6987"
+i=z.isnumeric()
+print(i)
+
+
+print(s.endswith('t'))
+
+
+print(s.startswith('u'))
+
